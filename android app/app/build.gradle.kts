eea107037bf5d50be70bfb8c6572a821648b38be
RenzoRCDev <u202412882@upc.edu.pe>
2025-10-17 18:46:16 -0500

    Listar con Imagen

--- android app/app/build.gradle.kts
+++ android app/app/build.gradle.kts
@@ -59,6 +59,14 @@ dependencies {
     
     // Coroutines para programación asíncrona
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // Retrofit y Gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHttp y Logging Interceptor
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
